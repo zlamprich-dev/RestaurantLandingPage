8389785255c787353f8e16de74e2716cd831a498
Zach Lamprich <zlamprich.dev@gmail.com>
2024-09-04 21:13:35 -0600

    Initial webpack and homepage setup

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+};
